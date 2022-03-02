98aacd6a4a417b22a967801800771c672627b16e
Pranav Sriram <pranav@DNa1ca567.SUNet>
2022-03-01 23:34:36 -0800

    Hand-specific features

--- feature_extraction.py
+++ feature_extraction.py
@@ -19,35 +19,61 @@ class ExtractHandFeatures:
             
             #For training change this line, don't need to flip (since images appear to be from back-facing camera) 
             #Convert cv2 BGR image to RGB image and flip (since image coming from front-facing camera)  
-            #processed = hands.process(cv2.flip(cv2.cvtColor(self.raw_image, cv2.COLOR_BGR2RGB), 1))
-            processed = hands.process(cv2.cvtColor(self.raw_image, cv2.COLOR_BGR2RGB))
+            processed = hands.process(cv2.flip(cv2.cvtColor(self.raw_image, cv2.COLOR_BGR2RGB), 1))
+            # processed = hands.process(cv2.cvtColor(self.raw_image, cv2.COLOR_BGR2RGB)) 
 
             #No hand detected (Figure out how we want to handle, 126 vector with all 0s?): 
             if not processed.multi_hand_landmarks: 
                 zeros = torch.tensor(np.array([0] * 126), dtype=torch.float32)
                 return zeros
             
-            feature_vector = [] 
-            #Could have one or two hands: 
-            for hand in processed.multi_hand_landmarks: 
-                for curr_landmark in hand.landmark: 
-                    x = curr_landmark.x 
-                    feature_vector.append(x)
+            feature_vector = []         
+            hands = [] 
 
-                    y = curr_landmark.y 
-                    feature_vector.append(y)
+            for idx, hand_handedness in enumerate(processed.multi_handedness):
+                hands.append(hand_handedness.classification[0].label)
+                
 
-                    z = curr_landmark.z
-                    feature_vector.append(z)
+            #Left hand is first 63, Right hand is last 63
+            #LEFT HAND ONLY CASE: 
+            if (len(hands) == 1 and hands[0] == "Left"):
+                for hand in processed.multi_hand_landmarks: 
+                    for curr_landmark in hand.landmark: 
+                        x = curr_landmark.x 
+                        feature_vector.append(x)
 
-            #If we have just one hand, zero out the remaining (to ensure constant vector size of 126)
-            #Might cause problems in one-hand case if we care which hand is visible/showing sign language
-            #Solution to this is to use processed.multi_handedness
-            if (len(feature_vector) == 63):
+                        y = curr_landmark.y 
+                        feature_vector.append(y)
+
+                        z = curr_landmark.z
+                        feature_vector.append(z)
                 zero_vector = [0] * 63 
                 feature_vector.extend(zero_vector)
+
+            #RIGHT HAND ONLY CASE: 
+            if (len(hands) == 1 and hands[0] == "Right"):
+                # print("Detected only right hand")
+                for hand in processed.multi_hand_landmarks: 
+                    for curr_landmark in hand.landmark: 
+                        x = curr_landmark.x 
+                        feature_vector.append(x)
+
+                        y = curr_landmark.y 
+                        feature_vector.append(y)
+
+                        z = curr_landmark.z
+                        feature_vector.append(z)
+                zero_vector = [0] * 63 
+                feature_vector = zero_vector + feature_vector
             
+            #BOTH HANDS CASE: 
+            if (len(hands) == 2):
+                # print("Detected both hands")
+                zeros = torch.tensor(np.array([0] * 126), dtype=torch.float32)
+                return zeros
+
             output = torch.tensor(np.array(feature_vector), dtype=torch.float32)
+            print(output)
             return output
 
 
